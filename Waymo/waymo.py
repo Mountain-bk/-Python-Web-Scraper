b898f32363d3a498d29273155131e009bb70f358
Ryo Yamamoto <67309797+Mountain-bk@users.noreply.github.com>
2020-09-11 18:30:05 +0900

    Update waymo.py

--- Waymo/waymo.py
+++ Waymo/waymo.py
@@ -1,33 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-# scraping waymo
 waymo_career = requests.get('https://waymo.com/joinus/').text
 waymo_soup = BeautifulSoup(waymo_career, 'lxml')
 
 def get_waymo_software_engineer_job_information():
-    # Software Engineer Category
-    software_engineer = waymo_soup.find('li', {'ng-if': "ctrl.categoryIsActive('Software Engineering', ['Mountain View, California, United States','New York, New York, United States','San Francisco, California, United States','Seattle, Washington, United States','Oxford, England, United Kingdom','Ann Arbor, Michigan, United States',])"})
-    get_waymo_job_information(software_engineer)
-
-def get_waymo_system_engineer_job_information():
-    # Systems Engineer Category
-    system_engineer = waymo_soup.find('li', {'ng-if': "ctrl.categoryIsActive('Systems Engineering', ['Mountain View, California, United States',])"})
-    get_waymo_job_information(system_engineer)
-
-def get_waymo_job_information(job_type):
     print("---Waymo Software Job Information---")
-    for job_link in job_type.find_all('a', {'class': 'ak-update-params'}, href=True):
-        # get the specific job link page
-        job_role_page = requests.get('https://waymo.com' + job_link['href']).text
+    software_engineer = waymo_soup.find('li', {'ng-if': "ctrl.categoryIsActive('Software Engineering', ['Mountain View, California, United States','New York, New York, United States','San Francisco, California, United States','Seattle, Washington, United States','Oxford, England, United Kingdom','Ann Arbor, Michigan, United States',])"})
+    with open('waymo-software-engineer.csv', 'w', newline='', encoding='UTF-8') as csv_file:
+        fieldnames = ['job-title', 'job-description']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+        for job_link in software_engineer.find_all('a', {'class': 'ak-update-params'}, href=True):
+            # get the specific job link page
+            job_role_page = requests.get('https://waymo.com' + job_link['href']).text
+            new_soup = BeautifulSoup(job_role_page, 'lxml')
+            job_title = new_soup.find('div', {'class': 'career__title'}).text
+            job_description = new_soup.find('div', {'class': 'career__description'}).text
 
-        new_soup = BeautifulSoup(job_role_page, 'lxml')
-        job_title = new_soup.find('div', {'class': 'career__title'}).text
-        job_description = new_soup.find('div', {'class': 'career__description'}).text
-        print("")
-        print("----------" + job_title.text + " (Waymo) ----------")
-        print(job_description)
-        print("")
+            # print to terminal
+            print("")
+            print("----------" + job_title + " (Waymo) ----------")
+            print(job_description)
+            print("")
+
+            # input to csv file
+            csv_writer.writerow({'job-title': job_title, 'job-description': job_description})
 
 get_waymo_software_engineer_job_information()
-get_waymo_system_engineer_job_information()
